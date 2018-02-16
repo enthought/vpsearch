7a9c3708e5691b0863803b58b97fec4154bf10f9
Peter Zahemszky <pzahemszky@enthought.com>
2018-02-16 15:18:52 +0000

    Add integration tests for vpsearch

---
+++ vpsearch/vpsearch/tests/test_vpsearch_cli.py
@@ -0,0 +1,82 @@
+import os
+import unittest
+
+from click.testing import CliRunner
+
+from vpsearch._cli import build, query
+
+SIMPLE_DB = """\
+>s0
+AAAAA
+>s1
+CCCCC
+>s2
+GGGGG
+>s3
+TTTTT
+>s4
+AACCC
+"""
+
+QUERY_EXACT_MATCH = """\
+>q0
+AAAAA
+"""
+
+EXPECTED_OUTPUT_EXACT_MATCH = """\
+q0	s0	100.00	5	0	0	1	5	1	5	0	25
+q0	s4	40.00	5	0	0	1	5	1	5	0	-2
+q0	s2	0.00	5	0	0	1	5	1	5	0	-20
+"""
+
+QUERY_ALMOST_MATCH = """\
+>q1
+GGGGT
+"""
+
+EXPECTED_OUTPUT_ALMOST = """\
+q1	s2	80.00	5	0	0	1	5	1	5	0	16
+"""
+
+
+class TestVpsearch(unittest.TestCase):
+
+    def _write_fasta(self, content, fname):
+        with open(fname,'w') as f:
+            f.write(content)
+
+    def test_query(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            database_fname = 'db.fasta'
+            db_dir = os.path.splitext(database_fname)[0]+'.db'
+            self._write_fasta(SIMPLE_DB, database_fname)
+
+            # check building the database without errors
+            result = runner.invoke(build, [database_fname])
+
+            self.assertIsNone(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+            # test looking up exact query
+            query_exact = 'query0.fasta'
+            self._write_fasta(QUERY_EXACT_MATCH, query_exact)
+
+            result = runner.invoke(query, [db_dir, query_exact])
+
+            self.assertIsNone(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+            self.assertEqual(EXPECTED_OUTPUT_EXACT_MATCH, result.output)
+
+            # test looking up an almost matching query
+            query_almost = 'query1.fasta'
+            self._write_fasta(QUERY_ALMOST_MATCH, query_almost)
+
+            result = runner.invoke(query, [db_dir, query_almost])
+
+            self.assertIsNone(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+            self.assertEqual(EXPECTED_OUTPUT_ALMOST, result.output)
+
