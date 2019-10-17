02299085ef091e523f0952b231ebe443236c0a44
Joris Vankerschaver <jvankerschaver@enthought.com>
2019-10-17 10:34:14 -0500

    MAINT: Regenerate bundles, add pip

--- ci/__main__.py
+++ ci/__main__.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import subprocess
 import sys
@@ -10,6 +11,15 @@ BUNDLES = {
 }
 DEVENV = "vpsearch"
 
+DEPENDENCIES = [
+    "click",
+    "cython",
+    "numpy",
+    "parasail",
+    "pip",
+]
+
+EDM_CONFIG = "ci/edm.yaml"
 
 @click.group()
 def cli():
@@ -30,6 +40,23 @@ def run_tests():
     _run_in_env(cmd, environment=DEVENV)
 
 
+@cli.command(name='regenerate-bundles')
+def regenerate_bundles():
+    for platform, bundle_fname in BUNDLES.items():
+        cmd = [
+            "edm",
+            "--config", EDM_CONFIG,
+            "--api-token", os.environ['HATCHER_TOKEN'],
+            "bundle",
+            "generate",
+            "--version", "3.6",
+            "--platform", platform,
+            "--bundle-format", "2.0",
+            "--output-file", bundle_fname,
+        ] + DEPENDENCIES
+        _run(cmd)
+
+
 def _get_platform_tag():
     supported = {
         'linux': 'rh6-x86_64',
@@ -48,11 +75,12 @@ def _install_bundle(fname, environment):
 
 def _install_package(environment):
     package_dir = Path(__file__).parents[1]
-    _run(['pip', 'install', '-e', str(package_dir)])
+    _run_in_env(['pip', 'install', '-e', str(package_dir)], environment)
 
 
 def _run(cmd, **kwds):
     kwds = {'check': True, **kwds}
+    click.secho(' '.join(cmd), fg='green')
     subprocess.run(cmd, **kwds)
 
 
