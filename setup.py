0383475618729ee1b8390c9245ca46d0ba68dc62
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2021-10-16 17:05:24 +0900

    MAINT: Fix version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ else:
 
 setup(
     name='vpsearch',
-    version='0.1.3.dev0',
+    version='0.2.0.dev0',
     author='Enthought',
     author_email='info@enthought.com',
     url='https://github.com/enthought/vpsearch',
